5c3da4f2048d80e2d8915d3b23340c4d6948a84f
Chris Perivolaropoulos <darksaga2006@gmail.com>
2013-05-12 20:48:27 +0300

    Initial

---
+++ models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.forms.extras.widgets import SelectDateWidget
+
+EXAM_TYPES = (
+    ('m', 'midterm'),
+    ('f', 'final'),
+    ('e', 'excercise'),
+    ('t', 'test'),
+)
+
+class Student(models.Model):
+    am = models.SmallIntegerField(primary_key=True) # XXX: max_value = 10000
+    date_enrolled = models.DateField('Date Enrolled')
+    semester = models.IntegerField(default=1)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    undergraduate = models.BooleanField()
+
+class SemesterSubject(models.Model):
+    students = models.ManyToManyField(Student)
+    name = models.CharField(max_length=100)
+    year = models.IntegerField()
+
+class Exam(models.Model):
+    subject = models.ForeignKey(SemesterSubject)
+    type = models.CharField(max_length=1, choices=EXAM_TYPES)
+    percent = models.FloatField()
+    question_set = models.FileField(upload_to="files/")
+
+class Grade(models.Model):
+    student = models.ForeignKey(Student)
+    grade = models.IntegerField(default=0)
+    exam = models.ForeignKey(Exam)
---
+++ views.py
@@ -0,0 +1,59 @@
+import datetime
+
+from django import forms
+from django.core.urlresolvers import reverse_lazy
+from django.forms.extras.widgets import SelectDateWidget
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+
+from profapp.models import Student
+from profapp.forms import StudentForm
+
+class SqlPresenterMixin(object):
+    """ Provide the sql query to the context.
+    """
+
+    def get_context_data(self, **kwargs):
+        context = super(SqlPresenterMixin, self).get_context_data(**kwargs)
+
+        context['sqlquery'] = str(self.get_queryset().query)
+        return context
+
+class StudentDetailView(SqlPresenterMixin, DetailView):
+    template_name = "profapp/student_details.html"
+    context_object_name = "student"
+    model = Student
+    slug_field = 'am'
+
+
+
+class StudentListView(SqlPresenterMixin, ListView):
+    template_name = "profapp/student_list.html"
+    context_object_name = "students"
+    model = Student
+
+
+class StudentMixin(object):
+    """ A mixin to do standard stuff.
+    """
+    context_object_name = "student"
+
+    def get_success_url(self):
+        """ Redirect to student view.
+        """
+        return "/students/%s/" % str(self.object.am)
+
+
+class StudentCreateView(SqlPresenterMixin, StudentMixin, CreateView):
+    form_class = StudentForm
+    template_name = "profapp/student_form.html"
+    slug_field = "am"
+
+class StudentUpdateView(SqlPresenterMixin, StudentMixin, UpdateView):
+    form_class = StudentForm
+    template_name = "profapp/student_form.html"
+    slug_field = "am"
+
+class StudentDeleteView(SqlPresenterMixin, DeleteView):
+    model = Student
+    slug_field = "am"
+    success_url = reverse_lazy('student-list')
