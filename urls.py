5471ad5e991acec8ba3cbab49a7cc0d2f2ba8190
fakedrake <darksaga2006@gmail.com>
2013-05-26 20:04:35 +0300

    Home is now just a student list

--- urls.py
+++ urls.py
@@ -7,12 +7,13 @@ from django.core.urlresolvers import reverse
 from profapp import views
 
 urlpatterns = patterns('',
-                       # url(r'^/$', RedirectView.as_view(url=reverse("student_list")), name="profapp_home"),
     url(r'^students/(?P<slug>\d+)/$', views.StudentDetailView.as_view(), name="student_view"),
     url(r'^students/$', views.StudentListView.as_view(), name="student_list"),
     url(r'^students/new/$', views.StudentCreateView.as_view(), name='student_create'),
     url(r'^students/(?P<slug>\d+)/update/$', views.StudentUpdateView.as_view(), name='update_student'),
     url(r'^students/(?P<slug>\d+)/delete/$', views.StudentDeleteView.as_view(), name='delete_student'),
+    url(r'^home/$', views.StudentListView.as_view(), name="profapp_home"),
+
                        #    url(r'^subjects/$', views.SemesterStudentListView.as_view()),
                        #    url(r'^submitsub/(?P<pk>\d+)/$', views.SemesterStudentCreateView.as_view()),
 )
