766a8627d41d79dcf14e4422abc3ff39772b936c
fakedrake <darksaga2006@gmail.com>
2013-05-26 19:03:32 +0300

    Bootstrap works on form

--- forms.py
+++ forms.py
@@ -2,9 +2,12 @@ from django import forms
 from django.forms import ModelForm
 
 from profapp.models import Student
+from bootstrap_toolkit.widgets import BootstrapDateInput, BootstrapTextInput, BootstrapUneditableInput
 
 class StudentForm(ModelForm):
+
+    date_enrolled = forms.DateField(widget=BootstrapDateInput(),)
+
     class Meta:
         model = Student
-#	fields = ['first_name', 'last_name', 'am', 'date_enrolled', 'semester', 'undergraduate']
-      
+        fields = ['first_name', 'last_name', 'am', 'date_enrolled', 'semester', 'undergraduate']
--- urls.py
+++ urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import patterns, url
+from django.conf import settings
 
 from profapp import views
 
 urlpatterns = patterns('',
-    url(r'^students/(?P<slug>\d+)/$', views.StudentDetailView.as_view(), name="student_view"),
-    url(r'^students/$', views.StudentListView.as_view(), name="student_list"),
-    url(r'^students/new/$', views.StudentCreateView.as_view()),
-    url(r'^students/(?P<slug>\d+)/update/$', views.StudentUpdateView.as_view(), name='update_student'),
-    url(r'^students/(?P<slug>\d+)/delete/$', views.StudentDeleteView.as_view(), name='delete_student'),
-#    url(r'^subjects/$', views.SemesterStudentListView.as_view()),
-#    url(r'^submitsub/(?P<pk>\d+)/$', views.SemesterStudentCreateView.as_view()),
+                       url(r'^students/(?P<slug>\d+)/$', views.StudentDetailView.as_view(), name="student_view"),
+                       url(r'^students/$', views.StudentListView.as_view(), name="student_list"),
+                       url(r'^students/new/$', views.StudentCreateView.as_view()),
+                       url(r'^students/(?P<slug>\d+)/update/$', views.StudentUpdateView.as_view(), name='update_student'),
+                       url(r'^students/(?P<slug>\d+)/delete/$', views.StudentDeleteView.as_view(), name='delete_student'),
+                       #    url(r'^subjects/$', views.SemesterStudentListView.as_view()),
+                       #    url(r'^submitsub/(?P<pk>\d+)/$', views.SemesterStudentCreateView.as_view()),
 )
