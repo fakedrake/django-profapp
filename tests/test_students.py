9eff3511b15bfb7d43d2f8bc97cd203245b84265
fakedrake <darksaga2006@gmail.com>
2013-05-26 16:02:31 +0300

    Renamed template files. Not reflected to the code

--- tests/test_students.py
+++ tests/test_students.py
@@ -7,7 +7,7 @@ from profapp.models import Student
 class TestStudent(TestCase):
     def setUp(self):
         self.c = Client()
-        Student.objects.create(am=7361, first_name="Chris",
+        Student.objects.create(am=2222, first_name="Chris",
                         last_name="Perivolas",
                         date_enrolled=datetime.date(year=2010, day=15,
                                                     month=2))
@@ -18,5 +18,25 @@ class TestStudent(TestCase):
 
     def test_list(self):
         r = self.c.get("/profapp/students/")
-        self.assertIn("7361", r.content)
+        self.assertIn("2222", r.content)
         self.assertIn("7362", r.content)
+
+    def test_details(self):
+        r = self.c.get("/profapp/students/2222/")
+        self.assertIn("2222", r.content)
+        self.assertIn("Chris", r.content)
+        self.assertIn("Perivolas", r.content)
+
+    def test_update(self):
+        """
+        """
+        r = self.c.post("/profapp/students/2222/update/",
+                        dict(am=7363, first_name="Chris",
+                             last_name="Perivolas",
+                             date_enrolled="3/15/2010",
+                             semester=2,
+                             undergraduate=1))
+
+        self.assertRedirects(r, "/profapp/students/7363/")
+        self.assertEquals(Student.objects.filter(am=7363).exists(), True)
+        self.assertEquals(Student.objects.filter(am=2222).exists(), False)
--- urls.py
+++ urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import patterns, url
 from profapp import views
 
 urlpatterns = patterns('',
-    url(r'^(?P<slug>\d+)/$', views.StudentDetailView.as_view(), name="student_view"),
+    url(r'^students/(?P<slug>\d+)/$', views.StudentDetailView.as_view(), name="student_view"),
     url(r'^students/$', views.StudentListView.as_view(), name="student_list"),
-    url(r'^submit_student/$', views.StudentCreateView.as_view()),
-    url(r'^students/update/(?P<slug>\d+)/$', views.StudentUpdateView.as_view(), name='update_student'),
-    url(r'^students/delete/(?P<slug>\d+)$', views.StudentDeleteView.as_view(), name='delete_student'),
+    url(r'^students/new/$', views.StudentCreateView.as_view()),
+    url(r'^students/(?P<slug>\d+)/update/$', views.StudentUpdateView.as_view(), name='update_student'),
+    url(r'^students/(?P<slug>\d+)/delete/$', views.StudentDeleteView.as_view(), name='delete_student'),
 #    url(r'^subjects/$', views.SemesterStudentListView.as_view()),
 #    url(r'^submitsub/(?P<pk>\d+)/$', views.SemesterStudentCreateView.as_view()),
 )
