2d93f1502e7bfb1f4fe6267fba03d10958965b27
fakedrake <darksaga2006@gmail.com>
2013-05-26 20:36:40 +0300

    Moved some stuff around to make it more tidy

--- urls.py
+++ urls.py
@@ -1,18 +1,17 @@
 from django.conf.urls import patterns, url
 from django.conf import settings
-from django.views.generic import RedirectView
 from django.core.urlresolvers import reverse
 
+from profapp.views import student
 
-from profapp import views
 
 urlpatterns = patterns('',
-    url(r'^students/(?P<slug>\d+)/$', views.StudentDetailView.as_view(), name="student_view"),
-    url(r'^students/$', views.StudentListView.as_view(), name="student_list"),
-    url(r'^students/new/$', views.StudentCreateView.as_view(), name='student_create'),
-    url(r'^students/(?P<slug>\d+)/update/$', views.StudentUpdateView.as_view(), name='update_student'),
-    url(r'^students/(?P<slug>\d+)/delete/$', views.StudentDeleteView.as_view(), name='delete_student'),
-    url(r'^home/$', views.StudentListView.as_view(), name="profapp_home"),
+    url(r'^students/(?P<slug>\d+)/$', student.StudentDetailView.as_view(), name="student_view"),
+    url(r'^students/$', student.StudentListView.as_view(), name="student_list"),
+    url(r'^students/new/$', student.StudentCreateView.as_view(), name='student_create'),
+    url(r'^students/(?P<slug>\d+)/update/$', student.StudentUpdateView.as_view(), name='update_student'),
+    url(r'^students/(?P<slug>\d+)/delete/$', student.StudentDeleteView.as_view(), name='delete_student'),
+    url(r'^home/$', student.StudentListView.as_view(), name="profapp_home"),
 
                        #    url(r'^subjects/$', views.SemesterStudentListView.as_view()),
                        #    url(r'^submitsub/(?P<pk>\d+)/$', views.SemesterStudentCreateView.as_view()),
