b94299095f2d76c31764ecc4cd8a608a11f79096
maria <maria@snf-49491.(none)>
2013-05-30 22:54:09 +0300

    update subject views. CreateView doesnt work.

--- views/semestersubject.py
+++ views/semestersubject.py
@@ -19,19 +19,25 @@ class SubjectYearDetailView(DetailView):
     template_name = "profapp/subject/subject_year_details.djhtml"
     context_object_name = "subject_year"
     model = SemesterSubject
-#    slug_field = ('name','year')
-    slug_field = 'name'
+    slug_field = ('name','year')
 
 class SubjectListView(ListView):
     template_name = "profapp/subject/subject_list.djhtml"
     context_object_name = "subjects"
     model = SemesterSubject
+    
+    def get_queryset(self):
+	return SemesterSubject.objects.values('name').distinct()
 
 class SubjectYearListView(ListView):
     template_name = "profapp/subject/subject_year_list.djhtml"
     context_object_name = "subjects_year"
     model = SemesterSubject
     slug_field = 'name'
+
+    def get_queryset(self):
+	
+	return SemesterSubject.objects.filter()
 
 
 class SemesterSubjectMixin(object):
@@ -40,26 +46,26 @@ class SemesterSubjectMixin(object):
     context_object_name = "subject_year"
 
     def get_success_url(self):
-        """ Redirect to semester_subjects view.
+        """ Redirect to subject_year view.
         """
-        return reverse('subject_year_view', kwargs={ 'slug':str(self.object.am)})
+        return reverse('subject_year_view', kwargs={ 'slug':str(self.object.name), 'pk':self.object.pk })
 
 class SemesterSubjectCreateView(SemesterSubjectMixin, CreateView):
     model = SemesterSubject
     form_class = SemesterSubjectForm
-    template_name = "profapp/subject_form.djhtml"
-    slug_field = "name"
+    template_name = "profapp/subject/subject_form.djhtml"
+    slug_field = ("name", "year")
 
 
 class SemesterSubjectUpdateView(SemesterSubjectMixin, UpdateView):
     model = SemesterSubject
     form_class = SemesterSubjectForm
-    template_name = "profapp/subject_form.djhtml"
+    template_name = "profapp/subject/subject_form.djhtml"
     slug_field = "name"
 
 class SemesterSubjectDeleteView(DeleteView):
     model = SemesterSubject
     slug_field = "name"
-    template_name = "profapp/subject_confirm_delete.djhtml"
-    success_url = reverse_lazy('semester_subjects_list')
+    template_name = "profapp/subject/subject_confirm_delete.djhtml"
+    success_url = reverse_lazy('subject_list')
 
