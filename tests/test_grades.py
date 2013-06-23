1477a4721755067372fecade9bb25d00f047504b
Chris Perivolaropoulos <darksaga2006@gmail.com>
2013-06-23 21:22:43 +0300

    Subject filter for grades

--- tests/test_grades.py
+++ tests/test_grades.py
@@ -12,36 +12,45 @@ class TestGrades(TestCase):
     def setUp(self):
         self.c = Client()
 
-	self.subj = SemesterSubject(name="Math", year=2010)
-	self.subj.save()
-	self.subj.students.add(*[Student.objects.create(am=i,
-                                           date_enrolled=datetime.datetime.now(),
-                                           semester=1,
-                                           first_name="Chodey",
-                                           last_name="McNumnuts") for i in xrange(MIN_AM_RANGE,MAX_AM_RANGE)])
-	self.subj.save()
+	self.math = SemesterSubject(name="Math", year=2010)
+        self.math.save()
+        self.math.students.add(*[Student.objects.create(am=i,
+                                                        date_enrolled=datetime.datetime.now(),
+                                                        semester=1,
+                                                        first_name="Chodey",
+                                                        last_name="McNumnuts") for i in xrange(MIN_AM_RANGE,MAX_AM_RANGE)])
+        self.math.save()
 
-	self.stu = self.subj.students.all()
+        self.physics = SemesterSubject(name="Physics", year=2010)
+        self.physics.save()
+        self.physics.students.add(*[Student.objects.get(am=i) for i in xrange(MIN_AM_RANGE,MAX_AM_RANGE)])
+        self.physics.save()
 
-        self.exam1 = Exam.objects.create(subject=self.subj, type='m', percent=20)
-	self.exam2 = Exam.objects.create(subject=self.subj, type='f', percent=30)
+
+	self.stu = self.math.students.all()
+
+        self.exam1 = Exam.objects.create(subject=self.math, type='m', percent=20)
+        self.exam2 = Exam.objects.create(subject=self.physics, type='f', percent=30)
 
         self.grds1 = [Grade.objects.create(student=s, grade=5, exam=self.exam1) for s in self.stu]
-	self.grds2 = [Grade.objects.create(student=s, grade=6, exam=self.exam2) for s in self.stu]
+        self.grds2 = [Grade.objects.create(student=s, grade=6, exam=self.exam2) for s in self.stu]
 
     def test_list_grades(self):
-	r = self.c.get("/profapp/grades/")
+        r = self.c.get("/profapp/grades/")
 
 	# Assert that all students were graded
-	for am in xrange(MIN_AM_RANGE, MAX_AM_RANGE):
-	    self.assertIn(str(am), r.content)
+        for am in xrange(MIN_AM_RANGE, MAX_AM_RANGE):
+            self.assertIn(str(am), r.content)
 
 	self.assertIn("Final", r.content)
-	self.assertIn("Midterm", r.content)
+        self.assertIn("Midterm", r.content)
 
     def test_list_get(self):
-	r = self.c.get("/profapp/grades/?exam=%s" % self.exam1.pk)
-	self.assertNotIn(self.exam2.get_type_display(), r.content)
+        r = self.c.get("/profapp/grades/?exam=%s" % self.exam1.pk)
+        self.assertNotIn(self.exam2.get_type_display(), r.content)
 
 	r = self.c.get("/profapp/grades/?stud=%s" % self.stu[0].pk)
         self.assertEqual(4, len(r.content.split("<tr>")))
+
+	r = self.c.get("/profapp/grades/?subj=%s" % self.physics.pk)
+        self.assertEqual(102, len(r.content.split("<tr>")))
