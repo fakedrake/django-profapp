48d1350cbb44a73c27f5bba92d674a070b3e5106
Chris Perivolaropoulos <darksaga2006@gmail.com>
2013-06-24 20:50:06 +0300

    Fixed

--- views/sorter.py
+++ views/sorter.py
@@ -11,17 +11,26 @@ class Sorter(object):
         self.columns = set(list(args) + kwargs.values())
         self.mapping = kwargs
 
+        if not self.default:
+            raise KeyError("Default must be a legit string.")
+
     def reverse_sort(self, arg):
         """
         True if reverse sorting.
         """
-        return arg[0] == "-"
+        if arg:
+            return arg[0] == "-"
+
+        return self.reverse_sort(self.default)
 
 
     def order_col(self, arg):
         """
         Return something order_by understands.
         """
+        if arg == None:
+            arg = self.default
+
         if self.reverse_sort(arg):
             col = arg[1:]
             tmpl = "-%s"
